2ae7ba09efe139dbc6998f32432ec09ee6ae70dc
iNe1t <fantiktop212@gmail.com>
2021-01-16 18:18:02 +0300

    commit

--- back.py
+++ back.py
@@ -1,4 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "Vova is a debil"
+
+if __name__ == "__main__":
+    app.run(debug=True)
