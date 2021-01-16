f9e6ad7a89ef5ceaf6b878752a1510f6aa344a24
iNe1t <fantiktop212@gmail.com>
2021-01-16 18:52:32 +0300

    commit

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/data")
+def function():
+    return render_template("data.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
