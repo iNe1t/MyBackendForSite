11a5ee33f0fa0b28903a2941ca9af6c9f4580a1f
iNe1t <fantiktop212@gmail.com>
2021-01-16 18:37:53 +0300

    commit

--- back.py
+++ back.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, url_for, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
