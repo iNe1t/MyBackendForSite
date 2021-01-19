9bb2a78c0005357dbe85339a689666f5bb423d4e
iNe1t <fantiktop212@gmail.com>
2021-01-19 18:00:03 +0300

    commit

--- static/script.js
+++ static/script.js
@@ -5,6 +5,7 @@ $("#button").click(function(){
     if (clicks === 1) {
         $("#menu").css("visibility", "visible"),
         $("#menu1").css("visibility", "visible")
+        $("body").css("overflow-y", "hidden")
     } else if (clicks ===2) {
         $("#menu").css("visibility", "hidden"),
         $("#menu1").css("visibility", "hidden")
