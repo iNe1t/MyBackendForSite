fff38e4eef26d0d49673a58b3807c3e024b815b4
iNe1t <fantiktop212@gmail.com>
2021-01-30 19:06:40 +0300

    commit

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def main_page():
     return render_template("index.html")
-
+@app.route("/articles")
+def article_maker_page():
+    return render_template("article.html")
 if __name__ == "__main__":
     app.run(debug=True)
