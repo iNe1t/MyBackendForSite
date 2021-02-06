aa1582e59fc460321087b1ec33b35ec2a6d9ab20
iNe1t <fantiktop212@gmail.com>
2021-02-06 19:18:00 +0300

    commit

--- ne1texclub/article_maker/models.py
+++ ne1texclub/article_maker/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
+title = models.TextField(max_length=30, default="Новая статья")
+text = models.TextField(max_length=500, default="Предположим тут есть текст. Не трогай его!")
+
 # Create your models here.
