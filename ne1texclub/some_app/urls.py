0b547ca81ba594fc382424e5039d8a7169187ef4
iNe1t <fantiktop212@gmail.com>
2021-02-06 17:58:44 +0300

    commit

--- ne1texclub/some_app/urls.py
+++ ne1texclub/some_app/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import index
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    #path('start/', include('some_app.urls'))
+    path('', index)
 ]
