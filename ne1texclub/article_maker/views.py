1072c579b5ed6cb6e1ab14f3b79480477e5203ce
iNe1t <fantiktop212@gmail.com>
2021-02-27 19:20:02 +0300

    commit

--- ne1texclub/article_maker/views.py
+++ ne1texclub/article_maker/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
-
+from .models import Post
+ 
+ 
 def article_maker(request):
-    return render(request, 'posts/articles.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, "posts/articles.html", context)
+ 
+ 
+def post_page(request, post_id):
+    context = {
+        'post': Post.objects.get(id=post_id)
+    }
+    return render(request, "posts/post.html", context)
